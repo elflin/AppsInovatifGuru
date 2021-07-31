9ff970bf1802ff441b9bd33bfa2f53e3f41b4a32
evantanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2021-07-31 22:18:56 +0700

    Splash Screen

---
+++ app/src/main/java/com/uc/appsinovatifguru/BeginlogoActivity.java
@@ -0,0 +1,36 @@
+package com.uc.appsinovatifguru;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+
+public class BeginlogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beginlogo);
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                checkOnboarding();
+                finish();
+            }
+        }, 3 * 1000);
+    }
+
+
+    private void checkOnboarding(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        // Check if we need to display our OnboardingSupportFragment
+        if (!sharedPreferences.getBoolean(GlobalValue.Onboarding_Complete, false)) {
+            // The user hasn't seen the OnboardingSupportFragment yet, so show it
+            startActivity(new Intent(this, OnboardingActivity.class));
+        }else{
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+}
---
+++ app/src/main/java/com/uc/appsinovatifguru/GlobalValue.java
@@ -0,0 +1,5 @@
+package com.uc.appsinovatifguru;
+
+public class GlobalValue {
+    static String Onboarding_Complete = "COMPLETED_ONBOARDING_PREF_NAME";
+}
