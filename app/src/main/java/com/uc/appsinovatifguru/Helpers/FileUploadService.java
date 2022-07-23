790df43e8f030742addf7375849c61f03df9d08c
Evan Tanuwijaya <evan.tanuwijaya@ciputra.ac.id>
2022-07-23 16:20:59 +0700

    Bug fix packagingOptions

--- app/src/main/java/com/uc/appsinovatifguru/Helpers/FileUploadService.java
+++ app/src/main/java/com/uc/appsinovatifguru/Helpers/FileUploadService.java
@@ -1,13 +1,11 @@
 package com.uc.appsinovatifguru.Helpers;
 
-import io.reactivex.Observable;
 import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
-import retrofit2.Call;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
+import rx.Observable;
 
 public interface FileUploadService {
     @Multipart
